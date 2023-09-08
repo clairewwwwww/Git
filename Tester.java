6e7395f28393f7e3da9d6df9f7b03f1a6412eeff
clairewwwwww <122925057+clairewwwwww@users.noreply.github.com>
2023-09-08 09:30:36 -0700

    more testers (MY CODE WORKS!!!!)

--- Tester.java
+++ Tester.java
@@ -3,8 +3,18 @@
 public class Tester {
     public static void main(String args[]) throws Exception
     {
-        Index tester = new Index("testCase");
-        tester.addBlob("fierkj");
+        Index tester = new Index("testCase1");
+        tester.addBlob("testCase1");
+        tester.addBlob("testCase2");
+        tester.removeBlob("testCase2");
+        tester.addBlob("testCase3");
+        tester.removeBlob("testCase1");
+        tester.addBlob("testCase1");
+        tester.addBlob("testCase2");
+        tester.removeBlob("testCase1");
+        tester.removeBlob("testCase2");
+        tester.removeBlob("testCase3");
+        tester.addBlob("testCase2");
     }
     
 }
