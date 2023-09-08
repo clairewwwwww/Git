7dd2ac8d5c5b55d8fd6fc8b1345301c8d722e34a
clairewwwwww <122925057+clairewwwwww@users.noreply.github.com>
2023-09-07 22:56:05 -0700

    testCase1 (worked)
    inputFile(testCase1) exist, create new "objects" folder and "index" file, add pair names to "index" and add new name (SHA1) + same content to "objects"

--- Index.java
+++ Index.java
@@ -2,8 +2,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-
-import java.io.FileWriter;
 import java.nio.file.Files;
 
 public class Index
--- Tester.java
+++ Tester.java
@@ -3,9 +3,8 @@
 public class Tester {
     public static void main(String args[]) throws Exception
     {
-        Index tester = new Index("test1");
-        tester.addBlob("test1");
-        //tester.removeBlob("test1");s
+        Index tester = new Index("testCase1");
+        tester.addBlob("testCase1");
     }
     
 }
