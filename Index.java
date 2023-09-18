790e9a8574fb0cc94346877ac321ee47091ccd9e
wyattlake <lakewyatt@gmail.com>
2023-09-18 12:38:01 -0700

    Added fixes to Blob and Index back

--- Index.java
+++ Index.java
@@ -3,6 +3,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Index {
     /*
@@ -21,21 +23,23 @@ public class Index {
      * Can remove Blobs given a filename
      * Removes the filename and Blob SHA1 from the key/value pair
      */
-    public Index() {
-        initialize("index");
+    public Index() throws IOException {
+        initialize();
     }
 
-    private void initialize(String fileName) {
-        File file = new File(fileName);
-
-        if (!file.exists()) {
-            File newFile = new File("index");
+    private void initialize() throws IOException {
+        Path indexPath = Paths.get("index");
+        Path objectsPath = Paths.get("objects/");
+        if (!Files.exists(indexPath)) {
+            Files.createFile(indexPath);
         }
 
-        new File("objects").mkdirs();
+        if (!Files.exists(objectsPath)) {
+            Files.createDirectory(objectsPath);
+        }
     }
 
-    public void addBlob(String fileName) throws IOException {
+    public void addBlob(String fileName) throws Exception {
         Blob blob = new Blob(fileName);
         blob.createFile();
 
@@ -69,8 +73,8 @@ public class Index {
         temp.renameTo(file);
     }
 
-    private String namePairs(String fileName) throws IOException {
+    private String namePairs(String fileName) throws Exception {
         Blob blob = new Blob(fileName);
-        return fileName + ": " + blob.getSHA1String();
+        return fileName + " : " + blob.getSHA1String();
     }
 }
