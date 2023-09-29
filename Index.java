f1b7aeff7810990b2c395ab05111c3c1ff7ff0a9
clairewwwwww <122925057+clairewwwwww@users.noreply.github.com>
2023-09-29 10:17:42 -0700

    changed back from previous commit

--- Index.java
+++ Index.java
@@ -43,7 +43,7 @@ public class Index {
         Blob blob = new Blob(fileName);
         blob.createFile();
 
-        PrintWriter pw = new PrintWriter(new FileWriter("index", false));
+        PrintWriter pw = new PrintWriter(new FileWriter("index", true));
         pw.append(namePairs(fileName));
         pw.append("\n");
         pw.close();
