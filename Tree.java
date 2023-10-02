dad5977ff09f7a905a95559aa90b1e6eb7f2bb4d
clairewwwwww <122925057+clairewwwwww@users.noreply.github.com>
2023-10-02 16:27:47 -0700

    update add tree method
    add three parts three

--- Tree.java
+++ Tree.java
@@ -31,18 +31,27 @@ public class Tree {
                 treeSet.add(splits[1]);
                 return;
             }
-        } else if (splits.length == 3) {
+        } else if (splits.length == 3 ) {
             // Adding a blob
-            if (splits[0].equals("blob")) {
+            if(splits[0].equals("blob"))
+            {
                 if (blobMap.containsKey(splits[2])) {
                     throw new Exception("Cannot add a blob with a duplicate filename");
                 }
                 blobMap.put(splits[2], splits[1]);
                 return;
             }
-        }
-
+            else if(splits[0].equals("tree"))
+            {
+                if(treeMap.containsKey(splits[2]))
+                {
+                    throw new Exception("Connot add a tree with a duplicate foldername");
+                }
+                treeMap.put(splits[2], splits[1]);
+                return;
+            }
         throw new Exception("Invalid add format");
+        }
     }
 
     public boolean remove(String key) {
