302010a8c72a8aa6a6d14cc9a91f8365534422a1
clairewwwwww <122925057+clairewwwwww@users.noreply.github.com>
2023-10-07 14:29:30 -0700

    created createTree method

--- Commit.java
+++ Commit.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.MessageDigest;
@@ -8,29 +9,63 @@ import java.security.NoSuchAlgorithmException;
 import java.util.LinkedList;
 
 public class Commit {
+    //private static final String Util = null;
     private LinkedList<File> list;
     private File commit;
     private String prevCommit;
-    private String treeSha;
+    private String current;
+    private String  treeSha;
 
     public Commit(String prevCommit, String author, String summary) throws Exception {
         this.prevCommit = prevCommit;
         commit = new File("Commit");
         PrintWriter pw = new PrintWriter(commit);
-        Tree tree = new Tree();
-        treeSha = tree.getSha1(Blob.readFile("index"));
+        //Tree tree = new Tree();
+        treeSha = createTree();
         String date = getDate();
-        pw.print(treeSha + '\n' + prevCommit + '\n' + '\n' + author + '\n' + date + '\n' + summary);
+        current = treeSha + '\n' + prevCommit + '\n' + '\n' + author + '\n' + date + '\n' + summary;
+        pw.print(current);
         pw.close();
-
-        String newEntry = "tree : " + tree.getSha1(Blob.readFile("Commit"));
-        tree.add(newEntry);
-        tree.writeToFile();
     }
 
-    
+    public String createTree() throws IOException, Exception
+    {
+        /*Tree tree = new Tree();
+        String content = Util.readFile("index");
+        content += "\ntree : " + treeSha;
+        String fileName = Util.hashString(content);
+        File file = new File("objects", fileName);
+        PrintWriter pw = new PrintWriter(new FileWriter(file), false);
+        pw.write(content);
+        pw.close();
+        Index index = new Index();
+        index.addBlob(fileName);*/
+        Tree tree = new Tree();
+        BufferedReader br = new BufferedReader(new FileReader("index"));
+        String content = "";
+        while(br.ready())
+        {
+            tree.add(br.readLine());
+            content += br.readLine();
+        }
+        br.close();
+        tree.add("tree : " + prevCommit);
+        tree.writeToFile();
+        content += "\ntree : " + prevCommit;
 
-    public void updatePrevious() throws IOException
+        String SHA = Util.hashString(content);
+
+        Util.writeFile(SHA, content);
+
+        PrintWriter overwrite = new PrintWriter(new FileWriter("index"), false);
+        overwrite.write("");
+        overwrite.close();
+
+        return SHA;
+
+    }
+
+    public void updatePrevious() throws IOException, NoSuchAlgorithmException
     {
         String path = "objects/" + prevCommit;
         File file = new File(path);
@@ -41,11 +76,12 @@ public class Commit {
             {
                 content += br.readLine();
             }
-            content += treeSha;
+            content += Util.hashString(current);
             while(br.ready())
             {
                 content += br.readLine();
             }
+            br.close();
         } 
         catch (IOException e) {
             // TODO Auto-generated catch block
